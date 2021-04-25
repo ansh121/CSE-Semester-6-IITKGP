335e132ce309e1981336c898bbf2adee8db37675
Anshul Choudhary <achoudhary718@gmail.com>
2021-04-25 14:00:21 +0530

    update

--- Operating Systems (CS30002)/Lab/OS_Assignment/Assgn1_1d_Group36_17CS10036_17CS30013.sh
+++
@@ -1,8 +0,0 @@
-mkdir "1.d.files.out"
-for file in "1.d.files"/*
-do
-	sort -nr $file > "1.d.files.out/$(basename "$file")"
-	echo "This File is sorted in reverse order $(basename "$file")"
-done
-cd "1.d.files.out"
-sort -m -r *.txt > "../1.d.out.txt"
